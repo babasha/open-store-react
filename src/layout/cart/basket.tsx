edd88323f0e216344ca29e93e5814436adcae626
EgorBabsuhkin <babushkin.e.ge@gmail.com>
2024-05-21 17:13:47 +0400

    Up

--- src/layout/cart/basket.tsx
+++ src/layout/cart/basket.tsx
@@ -1,41 +1,29 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import styled from 'styled-components';
 import { Container } from '../../components/Container';
 import { useCart } from './CartContext';
+import { useTranslation } from 'react-i18next';
 
 export const Basket: React.FC = () => {
   const { cartItems, removeItemFromCart, clearCart } = useCart();
-
-  useEffect(() => {
-    const handleCartCleared = () => {
-      document.querySelectorAll('.product-cart').forEach((cart) => {
-        (cart as any).reset();
-      });
-    };
-
-    document.addEventListener('cartCleared', handleCartCleared);
-
-    return () => {
-      document.removeEventListener('cartCleared', handleCartCleared);
-    };
-  }, []);
+  const { t } = useTranslation();
 
   return (
     <Container width={'auto'}>
       <CartdiInner>
-        <h2>Корзина</h2>
-        {cartItems.length === 0 && <p>Корзина пуста</p>}
+        <h2>{t('cart.title')}</h2>
+        {cartItems.length === 0 && <p>{t('cart.empty')}</p>}
         {cartItems.map((item) => (
           <CartItem key={item.id}>
             <span>{item.title}</span>
             <span>{item.quantity} кг</span>
             <span>{item.price} GEL</span>
-            <button onClick={() => removeItemFromCart(item.id)}>Удалить</button>
+            <button onClick={() => removeItemFromCart(item.id)}>{t('cart.remove')}</button>
           </CartItem>
         ))}
         {cartItems.length > 0 && (
           <>
-            <button onClick={clearCart}>Очистить корзину</button>
+            <button onClick={clearCart}>{t('cart.clear')}</button>
           </>
         )}
       </CartdiInner>
@@ -48,6 +36,7 @@ const CartdiInner = styled.div`
   padding: 20px;
   border-radius: 10px;
   box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+  
 `;
 
 const CartItem = styled.div`
