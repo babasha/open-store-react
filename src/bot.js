e8c14377b3dab5d0adac073ca99f6683ecb6d95c
EgorBabsuhkin <babushkin.e.ge@gmail.com>
2024-05-22 00:34:26 +0300

    up

--- src/bot.js
+++ src/bot.js
@@ -1,18 +1,18 @@
-// const TelegramBot = require('node-telegram-bot-api');
-// const token = 'YOUR_TELEGRAM_BOT_TOKEN'; 7124361098:AAHUee-cReFTQinJQBZSBI7wob2QOIgmFRA
-// const bot = new TelegramBot(token, { polling: true });
+const TelegramBot = require('node-telegram-bot-api');
+const token = '7124361098:AAHUee-cReFTQinJQBZSBI7wob2QOIgmFRA';  // Ваш токен
+const bot = new TelegramBot(token, { polling: true });
 
-// bot.onText(/\/start/, (msg) => {
-//   const chatId = msg.chat.id;
-//   const webAppUrl = 'https://your-web-app-url.com'; // Замените на URL вашего веб-приложения
+bot.onText(/\/start/, (msg) => {
+  const chatId = msg.chat.id;
+  const webAppUrl = 'https://babasha.github.io/open-store-react/'; // Замените на URL вашего веб-приложения
 
-//   const opts = {
-//     reply_markup: {
-//       inline_keyboard: [
-//         [{ text: 'Open Web App', web_app: { url: webAppUrl } }]
-//       ]
-//     }
-//   };
+  const opts = {
+    reply_markup: {
+      inline_keyboard: [
+        [{ text: 'Open Web App', web_app: { url: webAppUrl } }]
+      ]
+    }
+  };
 
-//   bot.sendMessage(chatId, 'Click the button to open the web app:', opts);
-// });
+  bot.sendMessage(chatId, 'Click the button to open the web app:', opts);
+});
