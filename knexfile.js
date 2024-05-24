ec78fe49e342863beb301f59641898a1abe0a485
EgorBabsuhkin <babushkin.e.ge@gmail.com>
2024-05-24 21:43:49 +0300

    up

--- knexfile.js
+++ knexfile.js
@@ -1,23 +1,17 @@
-// Update with your config settings.
-
-/**
- * @type { Object.<string, import("knex").Knex.Config> }
- */
 module.exports = {
-
   development: {
-    client: 'pg', // Используем PostgreSQL
+    client: 'pg',
     connection: {
-      host: '127.0.0.1', // Локальный хост
-      user: 'babasha', // Имя пользователя базы данных
-      password: 'dVP65PhHvXfGZXqQEQD2ZQCquLPjVX9t', // Пароль пользователя базы данных
-      database: 'openstore_ampi', // Имя базы данных
+      host: '127.0.0.1',
+      user: 'babasha',
+      password: 'dVP65PhHvXfGZXqQEQD2ZQCquLPjVX9t',
+      database: 'openstore_ampi',
     },
     migrations: {
-      directory: './migrations', // Путь к миграциям
+      directory: './migrations',
     },
     seeds: {
-      directory: './seeds', // Путь к семенам (начальным данным)
+      directory: './seeds',
     },
   },
 
@@ -25,32 +19,30 @@ module.exports = {
     client: 'postgresql',
     connection: {
       database: 'my_db',
-      user:     'username',
-      password: 'password'
+      user: 'username',
+      password: 'password',
     },
     pool: {
       min: 2,
-      max: 10
+      max: 10,
     },
     migrations: {
-      tableName: 'knex_migrations'
-    }
+      tableName: 'knex_migrations',
+    },
   },
 
   production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
     pool: {
       min: 2,
-      max: 10
+      max: 10,
     },
     migrations: {
-      tableName: 'knex_migrations'
-    }
-  }
-
+      directory: './migrations',
+    },
+    seeds: {
+      directory: './seeds',
+    },
+  },
 };
