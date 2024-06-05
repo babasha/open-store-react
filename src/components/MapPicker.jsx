1a8a83dfa5cd9ecc46c0e6a753d65260430280b3
EgorBabsuhkin <babushkin.e.ge@gmail.com>
2024-06-05 22:29:02 +0300

    up

--- src/components/MapPicker.jsx
+++ src/components/MapPicker.jsx
@@ -4,11 +4,18 @@ import 'leaflet/dist/leaflet.css';
 import L from 'leaflet';
 import axios from 'axios';
 import styled from 'styled-components';
+import markerIcon from './path-to-your-marker-icon.png.png'; // Импорт вашего значка
 
 const MapWrapper = styled.div`
   height: 300px;
-  width: 100%;
 `;
+
+const customIcon = new L.Icon({
+  iconUrl: markerIcon,
+  iconSize: [30, 30], // Размер значка
+  iconAnchor: [15, 30], // Точка привязки значка
+  popupAnchor: [0, -30], // Точка всплывающего окна
+});
 
 const MapPicker = ({ onAddressSelect }) => {
   const [position, setPosition] = useState(null);
@@ -16,12 +23,12 @@ const MapPicker = ({ onAddressSelect }) => {
 
   const fetchAddress = async (lat, lon) => {
     try {
-      const response = await axios.get(`https://nominatim.openstreetmap.org/reverse`, {
+      const response = await axios.get('https://nominatim.openstreetmap.org/reverse', {
         params: {
           lat,
           lon,
-          format: 'json'
-        }
+          format: 'json',
+        },
       });
       const address = response.data.display_name;
       setAddress(address);
@@ -36,18 +43,19 @@ const MapPicker = ({ onAddressSelect }) => {
       click(e) {
         setPosition(e.latlng);
         fetchAddress(e.latlng.lat, e.latlng.lng);
-      }
+      },
     });
 
     return position === null ? null : (
-      <Marker position={position}></Marker>
+      <Marker position={position} icon={customIcon}>
+      </Marker>
     );
   };
 
   return (
     <MapWrapper>
       <MapContainer center={[41.6168, 41.6367]} zoom={13} style={{ height: '100%', width: '100%' }}>
-      <TileLayer
+        <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
