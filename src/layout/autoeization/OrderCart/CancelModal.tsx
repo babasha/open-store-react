dda6b3fb6c52865ca207cb2e7f8ba74ccb0fbc57
EgorBabsuhkin <babushkin.e.ge@gmail.com>
2024-06-12 11:13:22 +0400

    up

--- src/layout/autoeization/OrderCart/CancelModal.tsx
+++ src/layout/autoeization/OrderCart/CancelModal.tsx
@@ -1,5 +1,6 @@
 // src/components/CancelModal.tsx
 import React from 'react';
+import { motion, useAnimation, PanInfo } from 'framer-motion';
 import styled from 'styled-components';
 
 interface CancelModalProps {
@@ -7,17 +8,38 @@ interface CancelModalProps {
   handleClose: () => void;
 }
 
-const CancelModal: React.FC<CancelModalProps> = ({ handleConfirmCancel, handleClose }) => (
-  <Modal>
-    <ModalContent>
-      <p>Время бесплатной отмены прошло. При отмене будет возвращено только 95% от общей суммы заказа.</p>
-      <ModalButton onClick={handleConfirmCancel}>Подтвердить</ModalButton>
-      <ModalButton onClick={handleClose}>Отмена</ModalButton>
-    </ModalContent>
-  </Modal>
-);
+const CancelModal: React.FC<CancelModalProps> = ({ handleConfirmCancel, handleClose }) => {
+  const controls = useAnimation();
 
-const Modal = styled.div`
+  const handleDragEnd = (event: Event, info: PanInfo) => {
+    if (info.offset.y > 100 || info.offset.y < -100) {
+      handleClose();
+    } else {
+      controls.start({ y: 0 });
+    }
+  };
+
+  return (
+    <Overlay>
+      <ModalContent
+        initial={{ opacity: 0, y: -50 }}
+        animate={{ opacity: 1, y: 0 }}
+        exit={{ opacity: 0, y: 50 }}
+        transition={{ type: 'spring', stiffness: 450, damping: 40 }}
+        drag="y"
+        dragConstraints={{ top: 0, bottom: 0 }}
+        dragElastic={0.2}
+        onDragEnd={handleDragEnd}
+      >
+        <p>Время бесплатной отмены прошло. При отмене будет возвращено только 95% от общей суммы заказа.</p>
+        <ModalButton onClick={handleConfirmCancel}>Подтвердить</ModalButton>
+        <ModalButton onClick={handleClose}>Отмена</ModalButton>
+      </ModalContent>
+    </Overlay>
+  );
+};
+
+const Overlay = styled(motion.div)`
   position: fixed;
   top: 0;
   left: 0;
@@ -29,7 +51,7 @@ const Modal = styled.div`
   background: rgba(0, 0, 0, 0.5);
 `;
 
-const ModalContent = styled.div`
+const ModalContent = styled(motion.div)`
   background: white;
   padding: 20px;
   border-radius: 10px;
