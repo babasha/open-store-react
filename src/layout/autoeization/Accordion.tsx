27ebc61021ea732ecc0c58e6de3d37d0d37b2199
EgorBabsuhkin <babushkin.e.ge@gmail.com>
2024-06-13 15:10:16 +0400

    up

--- src/layout/autoeization/Accordion.tsx
+++ src/layout/autoeization/Accordion.tsx
@@ -1,10 +1,9 @@
-// src/components/Accordion.tsx
 import React from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
 import OrderCard from './OrderCart/OrderCard';
 import { Order } from '../orderList/OrderList';
 import { AccordionHeader, OrderList, LoadMoreButton } from './styledauth/AccordionStyles';
-
+import { useTranslation } from 'react-i18next';
 
 interface AccordionProps {
   title: string;
@@ -15,36 +14,40 @@ interface AccordionProps {
   allOrdersCount: number;
 }
 
-const Accordion: React.FC<AccordionProps> = ({ title, isOpen, onClick, orders, loadMore, allOrdersCount }) => (
-  <motion.div layout>
-    <AccordionHeader onClick={onClick} aria-expanded={isOpen}>
-      {title}
-    </AccordionHeader>
-    <AnimatePresence initial={false}>
-      {isOpen && (
-        <motion.div
-          key={title}
-          initial="collapsed"
-          animate="open"
-          exit="collapsed"
-          variants={{
-            open: { opacity: 1, height: 'auto' },
-            collapsed: { opacity: 0, height: 0 },
-          }}
-          transition={{ duration: 0.5, ease: 'easeInOut' }}
-        >
-          <OrderList>
-            {orders.map(order => (
-              <OrderCard key={order.id} order={order} />
-            ))}
-          </OrderList>
-          {orders.length < allOrdersCount && (
-            <LoadMoreButton onClick={loadMore}>Загрузить еще</LoadMoreButton>
-          )}
-        </motion.div>
-      )}
-    </AnimatePresence>
-  </motion.div>
-);
+const Accordion: React.FC<AccordionProps> = ({ title, isOpen, onClick, orders, loadMore, allOrdersCount }) => {
+  const { t } = useTranslation();
+
+  return (
+    <motion.div layout>
+      <AccordionHeader onClick={onClick} aria-expanded={isOpen}>
+        {title}
+      </AccordionHeader>
+      <AnimatePresence initial={false}>
+        {isOpen && (
+          <motion.div
+            key={title}
+            initial="collapsed"
+            animate="open"
+            exit="collapsed"
+            variants={{
+              open: { opacity: 1, height: 'auto' },
+              collapsed: { opacity: 0, height: 0 },
+            }}
+            transition={{ duration: 0.5, ease: 'easeInOut' }}
+          >
+            <OrderList>
+              {orders.map(order => (
+                <OrderCard key={order.id} order={order} />
+              ))}
+            </OrderList>
+            {orders.length < allOrdersCount && (
+              <LoadMoreButton onClick={loadMore}>{t('load_more')}</LoadMoreButton>
+            )}
+          </motion.div>
+        )}
+      </AnimatePresence>
+    </motion.div>
+  );
+};
 
 export default Accordion;
