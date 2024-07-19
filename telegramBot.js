f5eec8ea58f2bf98bbb9fe063c1d20bff2c6ece3
EgorBabsuhkin <babushkin.e.ge@gmail.com>
2024-07-19 14:29:12 +0300

    up

--- telegramBot.js
+++ telegramBot.js
@@ -1,21 +1,21 @@
-// const TelegramBot = require('node-telegram-bot-api');
+const TelegramBot = require('node-telegram-bot-api');
+const token = '7246879435:AAEOX8oHojQQ5atQcD1rGIaPJEBr5hPifn4';
+const bot = new TelegramBot(token, { polling: true });
 
-// const token = 'YOUR_TELEGRAM_BOT_TOKEN'; // Замените на ваш токен
+bot.onText(/\/start/, (msg) => {
+  const chatId = msg.chat.id;
+  const url = 'https://enddel.com';
 
-// const bot = new TelegramBot(token, { polling: true });
-
-// let users = [];
-
-// bot.onText(/\/start/, (msg) => {
-//   const chatId = msg.chat.id;
-//   const username = msg.chat.username;
-
-//   if (!users.find(user => user.chatId === chatId)) {
-//     users.push({ chatId, username });
-//     bot.sendMessage(chatId, 'Welcome! You are successfully registered.');
-//   } else {
-//     bot.sendMessage(chatId, 'You are already registered.');
-//   }
-// });
-
-// module.exports = { bot, users };
+  bot.sendMessage(chatId, 'Открыть мини-приложение', {
+    reply_markup: {
+      inline_keyboard: [
+        [
+          {
+            text: 'Открыть',
+            web_app: { url: url }
+          }
+        ]
+      ]
+    }
+  });
+});
