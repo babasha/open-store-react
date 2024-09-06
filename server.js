bb66836bb7b6f40425b08f871be51fbc095e91dd
EgorBabsuhkin <babushkin.e.ge@gmail.com>
2024-09-06 04:52:37 +0300

    up

--- server.js
+++ server.js
@@ -773,6 +773,7 @@ app.get('/api/orders/me', isAuthenticated, async (req, res) => {
 
 console.log('BOG_CLIENT_ID:', process.env.BOG_CLIENT_ID);
 console.log('BOG_CLIENT_SECRET:', process.env.BOG_CLIENT_SECRET);
+console.log('hello:');
 
 
 // Маршрут для получения всех пользователей
